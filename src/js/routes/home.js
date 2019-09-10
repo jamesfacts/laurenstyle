acc6b3b94472735fd509a39c56705a1df1b13c1b
James White <james@jfwhite.org>
2019-09-10 15:28:13 -0700

    connect mailchimp form

--- src/js/routes/home.js
+++ src/js/routes/home.js
@@ -7,7 +7,7 @@ export default {
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
-    const emailForm = `<form action="TKTK" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    const emailForm = `<form action="https://colinbeavan.us20.list-manage.com/subscribe/post?u=036e19ab640b0668e258a31d2&amp;id=694d104a77" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
       <div id="mc_embed_signup_scroll" class="mc-field-container">
 
     		<div class="mc-field-group">
@@ -22,7 +22,7 @@ export default {
 
         <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 
-        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_25301234b690ac0d7777a70bc_3541d01f8a" tabindex="-1" value=""></div>
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_036e19ab640b0668e258a31d2_694d104a77" tabindex="-1" value=""></div>
 
         <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn mc-button">
       </div>
