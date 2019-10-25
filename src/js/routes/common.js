d63bca704d4ea2a07655ddd979b3f6b3e82206ad
James White <james@jfwhite.org>
2019-10-24 22:24:24 -0700

    WIP add images to webpack file loader

--- src/js/routes/common.js
+++ src/js/routes/common.js
@@ -1,4 +1,5 @@
 import viewport from '../util/viewport'
+import fbSource from '../../img/fb.png';
 
 export default {
   init() {
@@ -8,6 +9,22 @@ export default {
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
     const footerHTML = `© Colin Beavan ${ new Date().getFullYear() } &ensp;&ensp;&ensp;&ensp;&ensp; Photography: <a href="https://www.thomasbrunot.com/">Thomas Brunot</a>`;
-    jQuery('.site-footer p').html( footerHTML );
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/Image
+    const fbImg = new Image(28, 28);
+    const igImg = new Image(28, 28);
+    const twitterImg = new Image(28, 28);
+    fbImg.src = fbSource;
+    igImg.src = fbSource;
+    twitterImg.src = fbSource;
+    // element.appendChild(img);
+
+    // const socialHTML = '<li><a href="https://facebook.com">' + fbImg + '</a></li>';
+
+    let $fbAnchor = $('<a/>').attr('href', 'https://facebook.com');
+    $fbAnchor.append( fbImg );
+
+    $('.menu.menu-primary').append( $fbAnchor );
+    $('.site-footer p').html( footerHTML );
   },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require( 'path' );
 const webpack = require( 'webpack' );
 const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
 
+
 // Set different CSS extraction for editor only and common block styles
 const mainCSSPlugin = new ExtractTextPlugin( {
   filename: 'style.css',
@@ -36,6 +37,7 @@ module.exports = {
   output: {
     path: path.resolve( __dirname, 'dist' ),
     filename: 'index.js',
+    publicPath: '/wp-content/plugins/colinstyles/dist/',
   },
   watch: 'production' !== process.env.NODE_ENV,
   devtool: 'cheap-eval-source-map',
@@ -51,6 +53,18 @@ module.exports = {
       {
         test: /\.s?css$/,
         use: mainCSSPlugin.extract( extractConfig ),
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './img/'
+            }
+          }
+        ]
       },
     ],
   },
